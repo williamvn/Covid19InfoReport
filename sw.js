2352ccaee68b4c214dcf99ef9adcd1c18284c1c2
Willy <williamvn1995@gmail.com>
2020-07-02 13:15:02 +0200

    Manifest and Install Done

--- sw.js
+++ sw.js
@@ -1,1 +1,30 @@
 console.log("Logging Service Worker");
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open('mi-cache').then(function (cache) {
+            return cache.addAll([
+                './',
+                './index.html',
+                './index.js',
+                './style.css',
+            ]);
+        })
+    );
+});
+
+caches.open('mi-cache').then(function (cache) {
+    return cache.addAll([
+        './',
+        './index.html',
+        './index.js',
+        './style.css',
+    ])
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (response) {
+            return response || fetch(e.request);
+        })
+    );
+});
